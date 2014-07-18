d35f2b743149e99894dcb287c27e94319211d73c
Rusty Swayne <rusty@mindfly.com>
2014-07-18 09:33:15 -0700

    Bumped Buzz.Hybrid version to 1.3.0 with SmtpMailer addtion.

--- src/Buzz.Hybrid/Configuration/BuzzHybridVersion.cs
+++ src/Buzz.Hybrid/Configuration/BuzzHybridVersion.cs
@@ -11,7 +11,7 @@
         /// <summary>
         /// The version.
         /// </summary>
-        private static readonly Version Version = new Version("1.2.2");
+        private static readonly Version Version = new Version("1.3.0");
 
         /// <summary>
         /// Gets the current version of Buzz Hybrid.
